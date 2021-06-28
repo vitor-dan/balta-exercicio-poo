0e073efa887d7f10e4e0b44d90da7aa4b8c77ef6
Vitor Daniel <vitor_daniel85@outlook.com.br>
2021-06-28 20:02:11 -0300

    Inicio do projeto de mão na massa de OOP Balta

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
